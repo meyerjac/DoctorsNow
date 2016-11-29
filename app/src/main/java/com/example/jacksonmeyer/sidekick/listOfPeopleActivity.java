4fe8155c550f88532db6ed7a59f7bbf9e9aa9cc0
jackson Meyer <jacksonmeyer@jacksons-MBP.hsd1.or.comcast.net>
2016-11-28 20:44:35 -0800

    list view is working, array adapter is implemeted great, action bar dissapeared, I'm not sure why

--- app/src/main/java/com/example/jacksonmeyer/sidekick/listOfPeopleActivity.java
+++ app/src/main/java/com/example/jacksonmeyer/sidekick/listOfPeopleActivity.java
@@ -3,7 +3,12 @@ package com.example.jacksonmeyer.sidekick;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -11,6 +16,12 @@ import butterknife.ButterKnife;
 
 public class listOfPeopleActivity extends AppCompatActivity {
     @Bind(R.id.nameTextView) TextView mnameTextView;
+    @Bind(R.id.namesListView) ListView mnamesListView;
+    private String[] peopleList = new String[] {"Elon Musk", "Mother Teresa",
+            "Abe Lincoln", "Sophie LaCoste", "Brian Tesky", "Jess Bezos",
+            "Aaron Box", "Jackson Meyer", "Juneau Meyer",
+            "Megan Keough", "Amazing people", "Jesus",
+            "JB Straubel", "Dan keough"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +29,19 @@ public class listOfPeopleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_of_people);
         ButterKnife.bind(this);
 
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, peopleList);
+        mnamesListView.setAdapter(adapter);
+
+//        mnamesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String people = ((TextView)view).getText().toString();
+//                Toast.makeText(listOfPeopleActivity.this, people, Toast.LENGTH_LONG).show();
+//            }
+//        });
+
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
-        mnameTextView.setText("your name: " + name);
+        mnameTextView.setText("Hey " + name + " Here are the other people that are near you!");
     }
 }
