8504fd31b3a1fba4a20522fc127baccfb5e00e92
jackson Meyer <jacksonmeyer@jacksons-MBP.hsd1.or.comcast.net>
2016-11-29 01:12:09 -0800

    added a splash screen framework

---
+++ app/src/main/java/com/example/jacksonmeyer/sidekick/Splash.java
@@ -0,0 +1,52 @@
+package com.example.jacksonmeyer.sidekick;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by jacksonmeyer on 11/28/16.
+ */
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashh);
+
+        final ImageView iv = (ImageView) findViewById(R.id.ninja);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.abc_fade_out);
+
+
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                iv.startAnimation(an2);
+                finish();
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
+
