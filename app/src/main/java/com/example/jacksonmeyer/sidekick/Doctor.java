897ab709df9245d3f0e39f53bc2e1ee7c00cd552
jackson Meyer <jacksonmeyer@jacksons-MacBook-Pro.local>
2016-12-04 00:51:59 -0800

    have JSONDATA, just working on aprsing

--- app/src/main/java/com/example/jacksonmeyer/sidekick/Doctor.java
+++ app/src/main/java/com/example/jacksonmeyer/sidekick/Doctor.java
@@ -1,13 +1,16 @@
 package com.example.jacksonmeyer.sidekick;
 
+import java.util.ArrayList;
+
 public class Doctor {
     private String mFirstName;
     private String mLastName;
     private String mImage_url;
     private String mBio;
     private String mGender;
+    private ArrayList<String> mAddress = new ArrayList<>();
 
-    public Doctor(String mFirstName, String mLastName, String mImage_url, String mBio, String mGender) {
+    public Doctor(String mFirstName, String mLastName, String mImage_url, String mBio, String mGender, ArrayList<String> address) {
         this.mFirstName = mFirstName;
         this.mLastName = mLastName;
         this.mImage_url = mImage_url;
@@ -34,4 +37,9 @@ public class Doctor {
     public String getGender() {
         return mGender;
     }
+
+    public ArrayList<String> getAddress() {
+        return mAddress;
+    }
+
 }
--- app/src/main/java/com/example/jacksonmeyer/sidekick/DoctorService.java
+++ app/src/main/java/com/example/jacksonmeyer/sidekick/DoctorService.java
@@ -2,11 +2,20 @@ package com.example.jacksonmeyer.sidekick;
 
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class DoctorService {
 
@@ -27,5 +36,33 @@ public class DoctorService {
         Call call = client.newCall(request);
         call.enqueue(callback);
 
+    }
+
+    public ArrayList<Doctor> processResults(Response response) {
+        ArrayList<Doctor> doctors = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject betterDoctorJSON = new JSONObject(jsonData);
+                JSONArray doctorsJSON = betterDoctorJSON.getJSONArray("data");
+                for (int i = 0; i < doctorsJSON.length(); i++) {
+                    JSONObject doctor = doctorsJSON.getJSONObject(i);
+                    JSONArray practices = doctor.getJSONArray("practices");
+                    JSONObject profile = doctor.getJSONObject("profile");
+                    String firstName = profile.getString("first_name");
+                    String LastName = profile.getString("first_name");
+                    String firstName = profile.getString("first_name");
+
+                    Doctor doctorConstructor = new Doctor(firstName, lastName, imageUrl, bio, );
+                    doctors.add(doctorConstructor);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return doctors;
     }
 }
