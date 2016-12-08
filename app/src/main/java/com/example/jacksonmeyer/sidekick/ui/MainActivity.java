27bc5e75fb78b81a20d6892cbb61ab9e707587fc
jackson Meyer <jacksonmeyer@jacksons-MBP.hsd1.or.comcast.net>
2016-12-08 12:35:12 -0800

    add a menu button with drop down items

--- app/src/main/java/com/example/jacksonmeyer/sidekick/ui/MainActivity.java
+++ app/src/main/java/com/example/jacksonmeyer/sidekick/ui/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.jacksonmeyer.sidekick.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.jacksonmeyer.sidekick.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -32,6 +36,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(MainActivity.this, "Thanks for joining the team!", Toast.LENGTH_SHORT).show();
 
         }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        if (id == R.id.contactMe) {
+            navigateToContactPage();
+            return true;
+        }
+        if (id == R.id.test1) {
+            logout();
+            return true;
+        }
+        if (id == R.id.test2) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void navigateToContactPage() {
+        Intent intent = new Intent(MainActivity.this, JacksonContact.class);
+        startActivity(intent);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
 
